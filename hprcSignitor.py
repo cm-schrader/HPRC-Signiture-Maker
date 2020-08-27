f5f77be332c395f57c5d384f4df743a9df60b6bb
Christian M. Schrader <cmschrader@wpi.edu>
2020-08-26 22:28:44 -0400

    New Logo

--- hprcSignitor.py
+++ hprcSignitor.py
@@ -52,7 +52,7 @@ def create_signature(name, title, major, year):
         assetPath = pathlib.Path.cwd() / "assets"
         for fileName in os.listdir(assetPath):
             copyfile(assetPath / fileName, sigPath / fileName)
-        sigPath = sigPath / "AIAA.htm"
+        sigPath = sigPath / "HPRC.htm"
         copyfile("template.html", sigPath)
         with open(sigPath, "r") as sig:
             html = sig.read()
